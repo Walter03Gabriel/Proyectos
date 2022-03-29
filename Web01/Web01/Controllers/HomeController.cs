9f1a5a71aa87e5b2d2d969c27925ca2d5d776bf2
Walter Gabriel Aparicio Cachay <walterapacac@campus.monlau.com>
2022-03-29 18:50:50 +0200

    subiendo la paractica Web01

---
+++ Web01/Web01/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web01.Models;
+using Grpc.Core;
+using System.Net.Mail;
+using Web01.Services;
+
+namespace Web01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Vname = "Walter Aparicio de Monlau";
+            return View();
+        }
+        public IActionResult Experiencia()
+        {
+            
+            return View();
+        }
+        public IActionResult Estudios()
+        {
+
+            return View();
+        }
+        public IActionResult MisProyectos()
+        {
+            ViewBag.Vname = new RepositoryOfProjects().GetProjects();
+            return View();
+        }
+        public IActionResult Projects1()
+        {
+            return View();
+        }
+        public IActionResult Projects2()
+        {
+
+            return View();
+        }
+        public IActionResult Projects3()
+        {
+
+            return View();
+        }
+
+        public IActionResult MiCurriculum()
+        {
+            return View();
+        }
+
+        public IActionResult Contactar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult metodoEmail(ClassEmail classEmail) {
+            email(classEmail);
+            return View("Contactar");
+        }
+
+        private void email(ClassEmail classEmail)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add("walter.gabriel.03@gmail.com");
+            mail.From = new MailAddress(classEmail.email);
+            mail.Subject = classEmail.asunto;
+            mail.Body = classEmail.mensaje;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("minfo.network@gmail.com", "Minfo_Network03");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+
+        public IActionResult Habilidades()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web01/Web01/Services/RepositoryOfProjects.cs
@@ -0,0 +1,31 @@
+﻿using Web01.Models;
+
+namespace Web01.Services
+{
+    public class RepositoryOfProjects
+    {
+        public List<ClassProjects> GetProjects()
+        {
+            return new List<ClassProjects>() {
+            new ClassProjects {
+            Title = "Minfo Network",
+            Description = "Project to build a computer store",
+            Link = "/Home/Projects1",
+            ImageURL = "/img/Proyecto1.PNG"
+            },
+            new ClassProjects {
+            Title = "ARKAWARE",
+            Description = "Projects to create a video game store challenges",
+            Link = "/Home/Projects2",
+            ImageURL = "/img/Proyecto2.PNG"
+            },
+             new ClassProjects {
+            Title = "Vaul-Tec",
+            Description = "Project to create a cybersecurity company",
+            Link = "/Home/Projects3",
+            ImageURL = "/img/Proyecto3.PNG"
+            }
+            };
+        }
+    }
+}
